a33affa9e7b6d70e1ab9e0972f538313ebb60de1
Christian Melendez <christianhxc@gmail.com>
2019-06-15 21:37:07 +0200

    Updated the loadtest script to use hard coded values

--- src/lab15/loadgen.sh
+++ src/lab15/loadgen.sh
@@ -2,12 +2,7 @@
 set -e
 trap "exit" TERM
 
-if [[ -z "${FRONTEND_ADDR}" ]]; then
-    echo >&2 "FRONTEND_ADDR not specified"
-    exit 1
-fi
-
 pip install --user -r requirements.txt
 
 set -x
-locust --host="http://${FRONTEND_ADDR}" --no-web -c "${USERS:-30}" -r 10 -t 15m --print-stats
+locust --host="http://localhost" --no-web -c "30" -r 10 -t 15m --print-stats
